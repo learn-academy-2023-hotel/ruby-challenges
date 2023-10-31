f786227ec4036fbe1db6be78ec055a40b7cc80d9
JosephMcMorrisII <josephmcmorris14@gmail.com>
2023-10-31 16:23:43 -0700

    Finished all the challenges

--- conditionals-methods-jonathan-joseph.rb
+++ conditionals-methods-jonathan-joseph.rb
@@ -33,31 +33,65 @@
 #     end
 #    puts palindrome('reviver')
 
-puts 'Player one enter your name'
-user_one = gets.chomp
-puts'Player two enter your name'
-user_two = gets.chomp
+# puts 'Player one enter your name'
+# user_one = gets.chomp
+# puts'Player two enter your name'
+# user_two = gets.chomp
 
-puts 'player one enter either: rock, paper, or scissors'
-user_one_choice = gets.chomp.downcase
-puts 'player two enter either: rock, paper, or scissors'
-user_two_choice = gets.chomp.downcase
+# puts 'player one enter either: rock, paper, or scissors'
+# user_one_choice = gets.chomp.downcase
+# puts 'player two enter either: rock, paper, or scissors'
+# user_two_choice = gets.chomp.downcase
 
-puts user_one_choice
-puts user_two_choice
+# puts user_one_choice
+# puts user_two_choice
 
-def winner(one, two)
-    if one == 'rock' && two == 'scissors'
-        'Player One wins the round'
-    elsif one == 'scissors'&& two == 'paper'
-        'Player One wins the round'
-    elsif one == 'paper' && two == 'rock'
-        'Player One wins the round'
-    elsif one == two
-        'You both selected the same option. Try again'
+# def winner(one, two)
+#     if one == 'rock' && two == 'scissors'
+#         'Player One wins the round'
+#     elsif one == 'scissors'&& two == 'paper'
+#         'Player One wins the round'
+#     elsif one == 'paper' && two == 'rock'
+#         'Player One wins the round'
+#     elsif one == two
+#         'You both selected the same option. Try again'
+#     else
+#         'Player Two wins the round'
+#     end
+# end
+
+# puts winner(user_one_choice, user_two_choice)
+
+puts 'Create a User Name it must be at least 6 characters long and cannot contain !,#,$ or spaces.'
+user_id = gets.chomp.downcase
+puts 'Create a password it must be at least 6 characters long and must contain a !,#,$. Cannot be password. Must contain at least one number'
+password = gets.chomp.downcase
+
+def check_p(new_password)
+    if user_id == new_password
+        'Cannot be the same'
+    elsif new_password.length <= 6
+        'Password must be at least 6 characters.'
+    elsif new_password == "password"
+        'Your password cannot be password'
+    elsif new_password.to_i <= 1 
+        'Your password must include at least one number'
+        elsif new_password.exclude("!") && new_password.exclude("#") && new_password.exclude("$")
     else
-        'Player Two wins the round'
+        'Error try again'
     end
 end
 
-puts winner(user_one_choice, user_two_choice)
+def check_username(login)
+    if login == "password"
+        'Cannot be the same'
+    elsif login.length <= 6
+        'Login must be at least 6 characters.'
+    elsif login.include?("!") || login.include?("#") || login.include?("$")
+        'Your username cannont include a "!" , "#" , "$"'
+    else
+        'Error try again'
+    end
+end
+    
+puts check_username(user_id)
