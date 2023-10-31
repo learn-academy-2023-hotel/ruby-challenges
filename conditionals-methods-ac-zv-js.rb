da210ccdb8cfd8d239a70eb140acb49fa40a068c
zeniavarela <zvarela8@gmail.com>
2023-10-31 16:40:24 -0700

    part 3

--- conditionals-methods-ac-zv-js.rb
+++ conditionals-methods-ac-zv-js.rb
@@ -87,26 +87,65 @@
 
 # As a user, I can see a message in the terminal noting if there was a tie
 
-def game_results 
-    puts "Enter First User Name"
-    name1 = gets.chomp
-    puts "Enter Second User Name"
-    name2 = gets.chomp
-    puts "First User choose Rock, Paper, or Scissors?"
-    game1 = gets.chomp
-    puts "Second User choose Rock, Paper, or Scissors?"
-    game2 = gets.chomp
-    if game1 == game2
-        return 'it is a tie'
-    elsif game1 == "Rock" && game2 == "Scissors"
-        return "#{name1} is the winner"
-    elsif game1 == "Scissors" && game2 == "Paper"
-        return "#{name1} is the winner"
-    elsif game1 == "Paper" && game2 == "Rock"
-        return "#{name1} is the winner"
-    else
-        return "#{name2} is the winner"
+# def game_results 
+#     puts "Enter First User Name"
+#     name1 = gets.chomp
+#     puts "Enter Second User Name"
+#     name2 = gets.chomp
+#     puts "First User choose Rock, Paper, or Scissors?"
+#     game1 = gets.chomp
+#     puts "Second User choose Rock, Paper, or Scissors?"
+#     game2 = gets.chomp
+#     if game1 == game2
+#         return 'it is a tie'
+#     elsif game1 == "Rock" && game2 == "Scissors"
+#         return "#{name1} is the winner"
+#     elsif game1 == "Scissors" && game2 == "Paper"
+#         return "#{name1} is the winner"
+#     elsif game1 == "Paper" && game2 == "Rock"
+#         return "#{name1} is the winner"
+#     else
+#         return "#{name2} is the winner"
+#     end
+# end
+
+# p game_results
+
+
+# ✅ Challenge: Password Checker
+
+# As a developer, you have been tasked with creating the user registration for a secure web site. To test your code, enter the user ID and password in the terminal to find out if they are acceptable. The user ID and password must adhere to the to following criteria:
+# User ID and password cannot be the same.
+# User ID and password must be at least six characters long.
+# Password must contain at least one of: !#$
+# User ID cannot contain the following characters: !#$ or spaces
+# Password cannot be the word "password".
+# User password must contain at least one number.
+
+puts 'Enter User Name'
+    user_name = gets.chomp
+p user_name
+
+puts 'Enter Password'
+    password = gets.chomp
+p password
+
+def password_checker
+    if user_name == password
+        return 'User ID and password cannot be the same.'
+    elsif user_name.length < 6 || password.length < 6
+        return 'User ID and password must be at least six characters long.'
+    elsif !password.includes?['!' , '#' , '$' ]
+        return 'Password must contain at least one of: !#$'
+    elsif !user_name.includes?['!' , '#' , '$', ' ']
+        return 'User ID cannot contain the following characters: !#$ or spaces'   
+    elsif password != "password"
+        return 'Password cannot be the word "password".' 
+    elsif password.includes?[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]        
+          return 'Password must contain at least one number.'
+    else 
+        return 'Something went wrong.'      
     end
 end
 
-p game_results
+p password_checker
