a01e7ff5ebdb20833c9572b2ca5c91316fdd84ed
zeniavarela <zvarela8@gmail.com>
2023-10-31 11:46:32 -0700

    part 2

--- intro-adrian-zenia-judas.rb
+++ intro-adrian-zenia-judas.rb
@@ -63,3 +63,31 @@
 3.2.0 :026 > my_otherinteger
  => 338 
 3.2.0 :027 > 
+
+3.2.0 :018 > my_cohort = 'Hotel'
+ => "Hotel" 
+3.2.0 :019 > "My cohort name is #{my_cohort}"
+ => "My cohort name is Hotel" 
+3.2.0 :020 > my_cohort.upcase
+ => "HOTEL" 
+3.2.0 :021 > my_cohort.reverse
+ => "letoH" 
+3.2.0 :022 > my_cohort.include?('H')
+ => true 
+3.2.0 :023 > my_cohort.capitalize
+ => "Hotel" 
+3.2.0 :024 > my_cohort.delete
+(irb):24:in `delete': wrong number of arguments (given 0, expected 1+) (ArgumentError)
+	from (irb):24:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/gems/irb-1.8.1/exe/irb:9:in `<top (required)>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `load'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `eval'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `<main>'
+3.2.0 :025 > my_cohort.delete('H')
+ => "otel" 
+3.2.0 :026 > my_cohort.index('o')
+ => 1 
+3.2.0 :027 > my_cohort.swapcase
+ => "hOTEL" 
+3.2.0 :028 > 
