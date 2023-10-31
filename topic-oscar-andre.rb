3719e1d20067903e3778be07dfec1bf868b281ed
LEARN Academy <learnacademy@MacBook-Air.local>
2023-10-31 11:59:56 -0700

    2nd commit

--- topic-oscar-andre.rb
+++ topic-oscar-andre.rb
@@ -63,3 +63,60 @@ my_num * 26
  => 182 
 3.2.0 :031 > my_num
  => 182
+
+
+#  ======================================================================
+
+# Create a variable and return it in a sentence using string interpolation.
+3.2.0 :006 > "My name is#{my_name}"
+ => "My name isyogi" 
+3.2.0 :007 > "My name is #{my_name}"
+ => "My name is yogi" 
+3.2.0 :008 > 
+
+
+
+# Create a variable that contains a string and test some of the Ruby string methods:
+# .upcase
+# .reverse
+# .include?
+# .capitalize
+# .delete
+3.2.0 :006 > "My name is#{my_name}"
+ => "My name isyogi" 
+3.2.0 :007 > "My name is #{my_name}"
+ => "My name is yogi" 
+3.2.0 :008 > my_name.upcase
+ => "YOGI" 
+3.2.0 :009 > my_name.reverse
+ => "igoy" 
+3.2.0 :010 > my_name.include? 'i'
+ => true 
+3.2.0 :011 > my_name.capitalize
+ => "Yogi" 
+3.2.0 :012 > my_name.delete 'y'
+ => "ogi" 
+ # .index
+
+
+# .swapcase
+3.2.0 :014 > my_name.swapcase
+ => "YOGI" 
+3.2.0 :015 > my_name.swapcase
+ => "YOGI" 
+3.2.0 :016 > my_name.swapcase!
+ => "YOGI" 
+3.2.0 :017 > my_name.swapcase!
+ => "yogi" 
+3.2.0 :018 > 
+
+
+# Create an array that contains the name of at least five TV shows you enjoy.
+
+
+# Find the length of the array.
+# Return the first item in the array.
+# Return the fourth item in the array.
+# Permanently reverse the order of the array.
+# Create a new empty array for your top favorite TV shows.
+# Using the full TV show array, add your top two favorite shows to the empty array.
