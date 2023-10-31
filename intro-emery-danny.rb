1d725be6928272fb5b62a9c526f7fd59671100ab
EWinkeljohn <emcwink@gmail.com>
2023-10-31 11:57:14 -0700

    finished challenges

--- intro-emery-danny.rb
+++ intro-emery-danny.rb
@@ -56,3 +56,96 @@
  => "a word" 
 3.2.0 :023 > "We put #{word} in a sentence"
  => "We put a word in a sentence" 
+ 3.2.0 :001 > string = "Moonrise Kingdom"
+ => "Moonrise Kingdom" 
+3.2.0 :002 > string.upcase
+ => "MOONRISE KINGDOM" 
+3.2.0 :003 > string.reverse
+ => "modgniK esirnooM" 
+3.2.0 :004 > string.include?
+(irb):4:in `include?': wrong number of arguments (given 0, expected 1) (ArgumentError)
+	from (irb):4:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/gems/irb-1.8.1/exe/irb:9:in `<top (required)>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `load'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `eval'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `<main>'
+3.2.0 :005 > string.include?'o'
+ => true 
+3.2.0 :006 > string.include?'z'
+ => false 
+3.2.0 :007 > string.capitalize
+ => "Moonrise kingdom" 
+3.2.0 :008 > string.delete'k'
+ => "Moonrise Kingdom" 
+3.2.0 :009 > string.delete'K'
+ => "Moonrise ingdom" 
+3.2.0 :010 > string.index
+(irb):10:in `index': wrong number of arguments (given 0, expected 1..2) (ArgumentError)
+	from (irb):10:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/gems/irb-1.8.1/exe/irb:9:in `<top (required)>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `load'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `eval'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `<main>'
+3.2.0 :011 > string.index[1]
+(irb):11:in `index': wrong number of arguments (given 0, expected 1..2) (ArgumentError)
+	from (irb):11:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/gems/irb-1.8.1/exe/irb:9:in `<top (required)>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `load'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `eval'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `<main>'
+3.2.0 :012 > string.index'1'
+ => nil 
+3.2.0 :013 > string.index(1)
+(irb):13:in `index': no implicit conversion of Integer into String (TypeError)
+	from (irb):13:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/gems/irb-1.8.1/exe/irb:9:in `<top (required)>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `load'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `eval'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `<main>'
+3.2.0 :014 > string[1]
+ => "o" 
+3.2.0 :015 > string.swapcase
+ => "mOONRISE kINGDOM" 
+3.2.0 :016 > tv_shows = ["Bob's Burgers", 'Gossip Girl', 'Yellowjackets', 'Family Guy', 'Broad City']
+ => ["Bob's Burgers", "Gossip Girl", "Yellowjackets", "Family Guy", "Broad City"] 
+3.2.0 :017 > tv_shows.length
+ => 5 
+3.2.0 :018 > tv_shows.first
+ => "Bob's Burgers" 
+3.2.0 :019 > tv_shows[3]
+ => "Family Guy" 
+3.2.0 :020 > tv_shows.reverse!
+ => ["Broad City", "Family Guy", "Yellowjackets", "Gossip Girl", "Bob's Burgers"] 
+3.2.0 :021 > empty = []
+ => [] 
+3.2.0 :022 > empty << tv_shows[4, 0]
+ => [[]] 
+3.2.0 :023 > tv_shows[4, 0] << empty
+ => [[[]]] 
+3.2.0 :024 > empty
+ => [[]] 
+3.2.0 :025 > empty = []
+ => [] 
+3.2.0 :026 > empty << tv_shows[4]
+ => ["Bob's Burgers"] 
+3.2.0 :027 > empty << tv_shows[4] && tv_shows[0]
+ => "Broad City" 
+3.2.0 :028 > empty << tv_shows[4] & tv_shows[0]
+(irb):28:in `&': no implicit conversion of String into Array (TypeError)
+	from (irb):28:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/gems/irb-1.8.1/exe/irb:9:in `<top (required)>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `load'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `eval'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `<main>'
+3.2.0 :029 > empty
+ => ["Bob's Burgers", "Bob's Burgers", "Bob's Burgers"] 
+3.2.0 :030 > empty = []
+ => [] 
+3.2.0 :031 > empty << tv_shows[4] << tv_shows[0]
+ => ["Bob's Burgers", "Broad City"] 
+3.2.0 :032 > 
