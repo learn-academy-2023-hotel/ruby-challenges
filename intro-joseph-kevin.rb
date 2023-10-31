25e496867b9a6c8b0aa9f1b750eff4d74ac0b4e8
Joseph Kong <josephk1018@gmail.com>
2023-10-31 14:56:08 -0700

    ruby-intro-challenge completed

---
+++ intro-joseph-kevin.rb
@@ -0,0 +1,81 @@
+"3.2.0 :001 > 5 * 5
+ => 25 
+3.2.0 :002 > -6 % 3
+ => 0 
+3.2.0 :003 > 32 + 25  
+ => 57 
+3.2.0 :004 > total_age = 57
+ => 57 
+3.2.0 :005 > 57 - 28.5 
+ => 28.5 
+3.2.0 :006 > total_age % 2 
+ => 1 
+3.2.0 :007 > 5 / 0 
+(irb):7:in `/': divided by 0 (ZeroDivisionError)
+        from (irb):7:in `<main>'
+        from /Users/learnacademy/.rvm/gems/ruby-3.2.0/gems/irb-1.8.1/exe/irb:9:in `<top (required)>'
+        from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/IRB:25:in `load'
+        from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/IRB:25:in `<main>'
+        from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `eval'
+        from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `<main>'
+3.2.0 :008 > 5.0 / 5
+ => 1.0 
+3.2.0 :009 > 5.0 / 0 
+ => Infinity 
+ 3.2.0 :001 > 0 / 0
+(irb):1:in `/': divided by 0 (ZeroDivisionError)
+        from (irb):1:in `<main>'
+        from /Users/learnacademy/.rvm/gems/ruby-3.2.0/gems/irb-1.8.1/exe/irb:9:in `<top (required)>'
+        from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/IRB:25:in `load'
+        from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/IRB:25:in `<main>'
+        from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `eval'
+        from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `<main>'
+3.2.0 :002 > my_num = 2 
+ => 2 
+3.2.0 :003 > my_num /2 
+ => 1 
+3.2.0 :004 > my_num % 3
+ => 2 
+3.2.0 :005 > new_integer = 13
+ => 13 
+3.2.0 :006 > my_num <= new_integer
+ => true 
+3.2.0 :007 > my_num = 7 
+ => 7 
+3.2.0 :008 > new_integer = new_integer * 26 
+ => 338 
+3.2.0 :009 > "
+
+# first_name = 'Joseph'
+# p "Hi my name is #{first_name}!" 
+# new_string = 'This is a string!' 
+# p new_string.upcase 
+# print new_string
+# p new_string.reverse
+# puts new_string.reverse
+# p new_string.include?('s')
+# p new_string.capitalize 
+# p new_string.delete('string')
+# p new_string.index('s')
+# p new_string.swapcase 
+
+
+
+# Create an array that contains the name of at least five TV shows you enjoy.
+tv_shows = [ "the Chosen 1", "the Chosen 2", "the Chosen 3", "the Chosen 4", "the Chosen 5"]
+# puts tv_shows
+# # Find the length of the array.
+# puts tv_shows.length
+# # Return the first item in the array.
+# puts tv_shows[0]
+# # Return the fourth item in the array.
+# puts tv_shows[3]
+# # Permanently reverse the order of the array.
+# tv_shows.reverse!
+# puts tv_shows
+# # Create a new empty array for your top favorite TV shows.
+# new_array =[ ]
+# # Using the full TV show array, add your top two favorite shows to the empty array.
+# new_array << tv_shows[0]  
+# new_array << tv_shows[2]  
+# puts new_array
