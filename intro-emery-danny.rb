ef525c52715b1e93fa875805c9ae07e6ae1cb137
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-10-31 11:28:10 -0700

    intro challenge part 1

---
+++ intro-emery-danny.rb
@@ -0,0 +1,58 @@
+3.2.0 :001 > 2 + 2
+ => 4 
+3.2.0 :002 > 4 - 1
+ => 3 
+3.2.0 :003 > 4 * 2
+ => 8 
+3.2.0 :004 > 8 / 4
+ => 2 
+3.2.0 :005 > 8.0 + 1
+ => 9.0 
+3.2.0 :006 > 8.0 - 1
+ => 7.0 
+3.2.0 :007 > 8.0 * 2.2
+ => 17.6 
+3.2.0 :008 > 8.0 / 3.7
+ => 2.162162162162162 
+3.2.0 :009 > 8 % 2
+ => 0 
+3.2.0 :010 > 3 / 0
+(irb):10:in `/': divided by 0 (ZeroDivisionError)
+	from (irb):10:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/gems/irb-1.8.1/exe/irb:9:in `<top (required)>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `load'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `eval'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `<main>'
+3.2.0 :011 > 4.0 / 0
+ => Infinity 
+3.2.0 :012 > 0 / 0
+(irb):12:in `/': divided by 0 (ZeroDivisionError)
+	from (irb):12:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/gems/irb-1.8.1/exe/irb:9:in `<top (required)>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `load'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/irb:25:in `<main>'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `eval'
+	from /Users/learnacademy/.rvm/gems/ruby-3.2.0/bin/ruby_executable_hooks:22:in `<main>'
+3.2.0 :013 > fav_num = 4
+ => 4 
+3.2.0 :014 > fav_num / 2
+ => 2 
+3.2.0 :015 > fav_num % 3
+ => 1 
+3.2.0 :016 > friday_the = 13
+ => 13 
+3.2.0 :017 > friday_the > fav_num
+ => true 
+3.2.0 :018 > friday_the % fav_num
+ => 1 
+3.2.0 :019 > fav_num = 7
+ => 7 
+3.2.0 :020 > fav_num = fav_num * 26
+ => 182 
+3.2.0 :021 > fav_num
+ => 182 
+3.2.0 :022 > word = 'a word'
+ => "a word" 
+3.2.0 :023 > "We put #{word} in a sentence"
+ => "We put a word in a sentence" 
