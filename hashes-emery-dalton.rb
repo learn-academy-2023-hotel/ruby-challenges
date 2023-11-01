319152b4b1a7108ce46eb2a047f967425aad9f74
DaltonMcauliffe <dalton.j.mcauliffe97@gmail.com>
2023-11-01 16:35:22 -0700

    stretch challenges

--- hashes-emery-dalton.rb
+++ hashes-emery-dalton.rb
@@ -29,12 +29,19 @@ my_phone [:safari] = 'web browser'
 # my_phone[:apple_music] = my_phone.delete
 # {:tik_tok=>"social media to watch silly videos", :instagram=>"allows you to personally connect", :maps=>"guides you to the places you want to go"}
 
-my_phone.each do |key, value|
-    p "#{key} is a #{value}"
-end
+# my_phone.each do |key, value|
+#     p "#{key} is a #{value}"
+# end
 # "tik_tok is a social media to watch silly videos"
 # "spotify is a audio streaming platform"
 # "instagram is a watch personalized videos"
 # "maps is a shows your were you're going"
 # "safari is a web browser"
-
+def phone_caps hash 
+    hash.map do |key,value|
+    "#{key.capitalize}: #{value}"
+    end
+end
+#phone_caps = my_phone.transform_keys { |key| key.to_s.capitalize}
+p phone_caps my_phone
+ #phone_caps
