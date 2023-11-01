28a29b9fe2eb7ba234d126f4a7d200bff0a6e70f
anthonyholt <ant.scott227@gmail.com>
2023-10-31 21:17:27 -0700

    I finally got it

--- conditional-methods-ant-jean.rb
+++ conditional-methods-ant-jean.rb
@@ -44,23 +44,82 @@ def first_user name
     name 
 end
 
-puts 'Enter your name user1'
-user_name = gets.chomp
-p first_user user_name
+puts 'Enter your name User 1'
+    user_name = gets.chomp
+        # p first_user user_name
 
 def second_user name
     name 
 end
 
-puts 'Enter your name user2'
-user_name = gets.chomp
-p second_user user_name
+puts 'Enter your name User 2'
+    user2_name = gets.chomp
+        # p second_user user2_name
 
-puts "Rock,Paper,Scissors #{first_user}"
+first_user = first_user user_name
+second_user = second_user user2_name
+
+def first_user_choice choice
+    choice 
+end
+    puts "Rock, Paper, or Scissors #{first_user}?"
+        user_choice = gets.chomp
 
 
+def second_user_choice choice
+    choice 
+end
+    puts "Rock, Paper, or Scissors #{second_user}?"
+        user2_choice = gets.chomp
+
+first_choice = first_user user_choice
+second_choice = second_user user2_choice
 
 
-# puts 'Enter your name user two'
-# name = gets.chomp
-# puts "Hello, #{name}."
+# Next Level - Create a Hash for Rules
+
+    rules = {
+        "Rock" => "Scissors",
+        "Paper" => "Rock",
+        "Scissors" => "Paper"
+    }
+
+    if rules[first_choice] == second_choice
+        puts "#{first_user} Wins the Round!"
+    elsif first_choice == second_choice
+        puts "It's a Draw!"
+    else
+        puts "#{second_user} Wins this Round!"
+    end 
+
+# 2ND WAY TO SOLVE - CONDITIONAL
+
+
+    # if first_choice == "Rock" && second_choice == "Scissor" 
+    #     puts "#{first_user} Wins the Round!"
+
+    #     elsif first_choice == "Paper" && second_choice == "Rock"
+    #         puts "#{first_user} Wins the Round!"
+
+    #     elsif first_choice == "Scissors" && second_choice == "Paper"
+    #         puts "#{first_user} Wins the Round!"
+
+    # else
+
+    #     if second_choice == "Rock" && first_choice == "Scissor" 
+    #         puts "#{second_user_user} Wins the Round!"
+    
+    #     elsif second_choice == "Paper" && first_choice == "Rock"
+    #         puts "#{second_user} Wins the Round!"
+    
+    #     elsif second_choice == "Scissors" && first_choice == "Paper"
+    #         puts "#{second_user} Wins the Round!"
+
+    # else
+    #     if first_choice == second_choice
+    #         puts "It's a Draw!"
+    # end
+
+# end
+
+# end
