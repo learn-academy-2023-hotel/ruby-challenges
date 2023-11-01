d223da95c904291068e45a3ad184eb99b826e23e
EWinkeljohn <emcwink@gmail.com>
2023-11-01 15:09:16 -0700

    phone challenges

--- hashes-emery-dalton.rb
+++ hashes-emery-dalton.rb
@@ -3,12 +3,38 @@
 # {}
 
 my_phone [:tik_tok] = 'social media to watch silly videos'
-# my_phone [:spotify] = 'audio streaming platform'
-# my_phone [:instagram] = 'watch personalized videos'
-# my_phone [:maps] = "shows your were you're going"
-# my_phone [:safari] = 'web browser'
+my_phone [:spotify] = 'audio streaming platform'
+my_phone [:instagram] = 'watch personalized videos'
+my_phone [:maps] = "shows your were you're going"
+my_phone [:safari] = 'web browser'
 #p my_phone
 #{:tik_tok=>"social media to watch silly videos", :spotify=>"audio streaming platform", :instagram=>"watch personalized videos", :maps=>"shows your were you're going", :safari=>"web browser"}
 
-p my_phone[:tik_tok]
+# p my_phone[:tik_tok]
 #"social media to watch silly videos"
+
+# my_phone[:google_chrome] = my_phone.delete(:safari)
+# my_phone[:apple_music] = my_phone.delete(:spotify)
+# p my_phone  
+# {:tik_tok=>"social media to watch silly videos", :instagram=>"watch personalized videos", :maps=>"shows your were you're going", :google_chrome=>"web browser", :apple_music=>"audio streaming platform"}
+
+# my_phone[:maps] = 'guides you to the places you want to go'
+# my_phone[:instagram] = 'allows you to personally connect'
+# p my_phone
+# {:tik_tok=>"social media to watch silly videos", :spotify=>"audio streaming platform", :instagram=>"allows you to personally connect", :maps=>"guides you to the places you want to go", :safari=>"web browser"}
+
+# my_phone.delete(:google_chrome)
+# my_phone.delete(:apple_music)
+# p my_phone
+# my_phone[:apple_music] = my_phone.delete
+# {:tik_tok=>"social media to watch silly videos", :instagram=>"allows you to personally connect", :maps=>"guides you to the places you want to go"}
+
+my_phone.each do |key, value|
+    p "#{key} is a #{value}"
+end
+# "tik_tok is a social media to watch silly videos"
+# "spotify is a audio streaming platform"
+# "instagram is a watch personalized videos"
+# "maps is a shows your were you're going"
+# "safari is a web browser"
+
