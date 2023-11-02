87daf3af2ed7bbe64ec34135e2324ca925df9ea4
Justin Pinkard <justin.d.pinkard@gmail.com>
2023-11-02 11:19:26 -0700

    2nd commit

--- classes-justin-zenia-jonathan.rb
+++ classes-justin-zenia-jonathan.rb
@@ -1,41 +1,41 @@
-# ❤️ Challenges
+# # ❤️ Challenges
 
-# For the following Task challenge use initialize, setter, and getter methods for your class.
+# # For the following Task challenge use initialize, setter, and getter methods for your class.
 
-# As a developer, I can create a class called Task.
-# class Task 
+# # As a developer, I can create a class called Task.
+# # class Task 
+# # end
+
+# # As a developer, I can create three instances (objects) of class Task. e.g laundry = Task.new
+
+# # As a developer, I can initialize each instance of class Task with a title.
+# # As a developer, I can see the title of each instance of class Task.
+# class Task
+#   def initialize (laundry, dishes, trash)
+#     @laundry = laundry
+#     @dishes = dishes
+#     @trash = trash
+#     @incomplete = true
+#   end
+#   def get_laundry
+#     @laundry
+#   end
+#   def get_dishes
+#     @dishes
+#   end
+#   def get_trash
+#     @trash
+#   end
+#   def incomplete 
+#     @incomplete = false
+#   end
 # end
 
-# As a developer, I can create three instances (objects) of class Task. e.g laundry = Task.new
 
-# As a developer, I can initialize each instance of class Task with a title.
-# As a developer, I can see the title of each instance of class Task.
-class Task
-  def initialize (laundry, dishes, trash)
-    @laundry = laundry
-    @dishes = dishes
-    @trash = trash
-    @incomplete = true
-  end
-  def get_laundry
-    @laundry
-  end
-  def get_dishes
-    @dishes
-  end
-  def get_trash
-    @trash
-  end
-  def incomplete 
-    @incomplete = false
-  end
-end
-
-
-chores = Task.new('Justin has to do the laundry', 'Zenia has to do the dishes', 'Jonathan has to take out the trash')
-  p chores
-p chores.incomplete
-p chores
+# chores = Task.new('Justin has to do the laundry', 'Zenia has to do the dishes', 'Jonathan has to take out the trash')
+#   p chores
+# p chores.incomplete
+# p chores
 
 
 
@@ -48,10 +48,39 @@ p chores
 # For the following ColorPalette challenge use initialize and attr_accessor methods in your class.
 
 # As a developer, I can create a class called ColorPalette.
+# class ColorPalette
+#   def set_color_palette(color1)
+#     @color1 = color1
+#     @color2 = color2
+#     @color3 = color3
+#   end
+# end
+
 # As a developer, I can create three instances (objects) of class ColorPalette.
-# e.g green = ColorPalette.new
+# green = ColorPalette.new
+
 # As a developer, I can initialize each instance of the class ColorPalette with three colors.
-# e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+class ColorPalette
+  attr_accessor :color1, :color2, :color3
+  def initialize(color1,color2,color3)
+    @color1 = color1
+    @color2 = color2
+    @color3 = color3
+  end
+  def all_colors
+    "The palette colors are #{color1}, #{color2}, and #{color3}."
+  end
+end
+green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
 # As a developer, I can print the value of each individual color.
+# p green
+
+
 # As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
+
+# p green.all_colors
+
 # As a developer, I can change one or more colors of a given palette.
+
+green.color1 = "blue"
+p green.color1
