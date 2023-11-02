6169990c30862156290d583f1c99aa6ddcdccf65
EWinkeljohn <emcwink@gmail.com>
2023-11-02 16:40:24 -0700

    complete challenges

--- inheritance-andre-emery.rb
+++ inheritance-andre-emery.rb
@@ -66,11 +66,56 @@ p salmon.dead_fish
 p salmon
 
 # As a developer, if my Salmon reaches the ripe old age of 4, I can make it die peacefully after a full and happy life.
-
-
-
-
 # Hint: You will need a method that changes the status of alive in the initialize method of Animal.
+class Mammal < Animals
+    attr_accessor :warm_blooded
+    def initialize(age)
+        super(age)
+        @warm_blooded = true
+    end
+end 
+
+class Bear < Mammal
+    attr_accessor :age
+    def initialize(age)
+        super(age)
+    end 
+    def bear_info
+        "Alive:#{alive} Age:#{age} Warm-blooded:#{warm_blooded}" 
+    end
+    def dead_bear
+        if @age >= 20
+            @alive = false
+       'dead bear'
+        end
+    end
+end
+
+bear = Bear.new(19)
+bear.age_up
+bear.dead_bear
+# Output --> 2
+p bear.bear_info
+
+class Giraffe < Mammal
+    attr_accessor :name, :age, :height, :weight
+    def initialize(name, age, height, weight)
+        super(age)
+        @name = name
+        @height = height
+        @weight = weight
+    end
+    def giraffe_info
+        "#{name} the giraffe came in at #{height} and #{weight}!"
+    end
+end
+
+zuri = Giraffe.new("Zuri", 0, "3 ft.", "180 lbs")
+p zuri.giraffe_info
+# Output --> "Zuri the giraffe came in at 3 ft. and 180 lbs!"
+# Output --> #<Giraffe:0x000000010ddc65a0 @alive=true, @age=0, @warm_blooded=true, @name="Zuri", @height="3 ft.", @weight="180 lbs">
+
+
 # As a developer, I can create a Mammal that inherits from Animal.
 # As a developer, I can initialize all of my Mammals to be warm_blooded.
 # As a developer, I can create a Bear that inherits from Mammal.
