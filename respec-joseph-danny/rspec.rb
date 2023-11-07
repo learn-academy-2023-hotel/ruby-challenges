422949860c78c76f489116c45de8b384c46c8b36
JosephMcMorrisII <josephmcmorris14@gmail.com>
2023-11-07 14:13:03 -0800

    2nd push

--- respec-joseph-danny/rspec.rb
+++ respec-joseph-danny/rspec.rb
@@ -1,6 +1,14 @@
 class Task 
-attr_accessor :title
-    def initialize title
+# attr_accessor :title, :description, :status
+    def initialize title, description
         @title = title
+        @description = description
+        @status = 'in progress'
+    end
+
+    def change_status
+        @status = 'done'
     end
 end
+task = Task.new ('laundry','fold clothes')
+p task.status
--- respec-joseph-danny/rspec_spec.rb
+++ respec-joseph-danny/rspec_spec.rb
@@ -12,5 +12,19 @@ it 'has a title' do
     expect(my_task.title).to be_a (String)
     expect(my_task.title).to eq ('laundry')
     end
+
+it 'has a description' do
+        my_task = Task.new
+        my_task.description = 'fold clothes'
+        expect(my_task.description).to be_a (String)
+        expect(my_task.description).to eq ('fold clothes')
+    end
+
+    it 'has a status' do
+        my_task = Task.new
+        expect(my_task.status).to eq 'in progress'
+        expect(my_task.status).to be_a (String)
+        my_task.status = 'Done'
+    end
 end
 
