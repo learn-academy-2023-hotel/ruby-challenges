3517b3cd04f1bf134521bbfd767757942efc9c77
alexnguyen <alexanderdnguyen4@gmail.com>
2023-11-07 13:33:55 -0800

    Completed more steps in challenge

--- rspec-emery-alex/rspec.rb
+++ rspec-emery-alex/rspec.rb
@@ -5,15 +5,30 @@
 # As a developer, I can create a Task.
 
 class Task
-    attr_accessor :title
+    attr_accessor :title, :description, :progress
     def initialize
         @title = title
+        @description = description
+        @progress = 'in progress'
+    end
+    
+    def status
+        @progress = 'done'
     end
 end
 
 # As a developer, I can give a Task a title and retrieve it.
 
+# task = Task.new
+# task.title = 'Chores'
+# p task.title
+
 # As a developer, I can give a Task a description and retrieve it.
+
+# task.description = 
+
 # As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
+
+
 # As a developer, when I print a Task that is done, its status is shown.
 # As a developer, I can give a Task a due date. Hint: Use the built-in Ruby Date class.
--- rspec-emery-alex/rspec_spec.rb
+++ rspec-emery-alex/rspec_spec.rb
@@ -12,4 +12,22 @@ describe 'Task' do
         expect(my_task.title).to be_a String
         expect(my_task.title).to eq 'Chores'
     end
+
+    it 'has a description' do
+        my_task = Task.new
+        my_task.description = 'Things I have to do'
+        expect(my_task.description).to be_a String
+        expect(my_task.description).to eq 'Things I have to do'
+    end
+
+    it 'has a start' do
+        my_task = Task.new
+        expect(my_task.progress).to be_a String
+        expect(my_task.progress).to eq 'in progress'
+    end
+
+    it 'changes status' do
+        my_task = Task.new
+        expect{my_task.status}.to change{my_task.progress}.from('in progress').to('done')
+    end
 end
