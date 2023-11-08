e6347fdae4a869f5e3ce8d4c36254ba27cf62fc7
jonathanraya <jonathanraya79@gmail.com>
2023-11-07 18:16:25 -0600

    Finished challenges, started stretch goals.

--- respec-joseph-danny/rspec.rb
+++ respec-joseph-danny/rspec.rb
@@ -1,14 +1,32 @@
+require 'date'
 class Task 
-# attr_accessor :title, :description, :status
-    def initialize title, description
+attr_accessor :title, :description, :status, :due
+    def initialize
         @title = title
         @description = description
         @status = 'in progress'
+        @due = Date.parse('2023-11-7')
     end
 
     def change_status
         @status = 'done'
     end
+    def due_date
+        if @due == Date.today
+            'Task is due today.'
+        # @due = Date.parse('2023-11-7')
+        else 
+            "Task is due #{@due}"
+        end
+    end
 end
-task = Task.new ('laundry','fold clothes')
-p task.status
+
+
+# task = Task.new('laundry','fold clothes')
+# p task.status
+# task.change_status
+# p task.status
+# task.due_date
+# date = Date.parse('8th Nov 2023')
+# p task.due_date
+
--- respec-joseph-danny/rspec_spec.rb
+++ respec-joseph-danny/rspec_spec.rb
@@ -1,5 +1,7 @@
 require 'rspec'
 require_relative 'rspec'
+require 'date'
+require_relative 'task_list'
 
 describe Task do
 it 'has to be real' do
@@ -26,5 +28,26 @@ it 'has a description' do
         expect(my_task.status).to be_a (String)
         my_task.status = 'Done'
     end
+    it 'has a due date' do
+        my_task = Task.new
+        my_task.due = Date.parse('2023-11-7')
+        expect(my_task.due).to eq Date.parse('2023-11-7')
+        expect(my_task.due).to be_a(Date)
+    end
 end
 
+describe 'Task_list' do
+    it 'should exist' do
+    expect { Task_list.new }.to_not raise_error
+    end
+    it 'has an array of tasks' do
+        my_task_list = Task_list.new
+        walk = Task.new
+        clean = Task.new
+        my_task_list.add_task walk
+        my_task_list.add_task clean
+        expect(my_task_list.list).not_to be_empty
+        expect(my_task_list.list).to contain_exactly(walk, clean)
+        expect(my_task_list.list).to be_a Array
+    end
+end
