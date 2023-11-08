99e0ff992ef3a2e52f37f0bbd5688bf5bd2eecd3
alexnguyen <alexanderdnguyen4@gmail.com>
2023-11-07 16:36:20 -0800

    Started stretch goals

--- rspec-emery-alex/rspec_spec.rb
+++ rspec-emery-alex/rspec_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require_relative 'rspec.rb'
 require 'date'
+require_relative 'Tasklist.rb'
 
 describe 'Task' do
     it 'should exist' do
@@ -37,5 +38,22 @@ describe 'Task' do
         my_task.deadline due_date
         expect(my_task.due_date).to be_a Date
         expect(my_task.due_date.to_s).to eq '2023-11-08'
+    end
+end
+
+describe 'Tasklist' do
+    it 'should exist' do
+    expect{Tasklist.new}.to_not raise_error
+    end
+
+    it 'has array of tasks' do
+    new_task = Tasklist.new
+    laundry = Task.new
+    dishes = Task.new
+    new_task.add_item laundry
+    new_task.add_item dishes
+    expect(new_task.list).not_to be_empty
+    expect(new_task.list).to contain_exactly(laundry, dishes)
+    expect(new_task.list).to be_a Array
     end
 end
